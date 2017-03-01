b062e04c707144dbc4678bf273289c9917d8a0c3
Kateryna Protsenko <kpr@sitecore.net>
2017-03-01 22:42:54 +0200

    Fix for the issue "Querystring of the General Link field gets encoded"

---
+++ src/Sitecore.Support.82835/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.82835")]
+[assembly: AssemblyProduct("Sitecore.Support.82835")]
+[assembly: ComVisible(false)]
